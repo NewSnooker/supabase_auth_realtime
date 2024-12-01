c8822cd8c800be1e193956f29f90bf803b43c142
NewSnooker <nu0075533@gmail.com>
2024-12-02 04:38:00 +0700

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // ให้แน่ใจว่า Base URL ถูกต้อง
+});
